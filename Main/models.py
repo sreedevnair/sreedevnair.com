4efa2df01d4694ee92d6f381e16b1cd15701afbf
Sreedev Nair <sreedevnair02@gmail.com>
2024-08-17 17:56:07 +0530

    feat: created WebsiteDetail model

--- Main/models.py
+++ Main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class WebsiteDetail(models.Model):
+
+    first_name = models.CharField(max_length=12)
+    second_name = models.CharField(max_length=12)
+    about_me = models.TextField()
+    profile_picture = models.ImageField(upload_to='pics/')
+
+    linked_by = models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.linked_by
--- Main/views.py
+++ Main/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import WebsiteDetail
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
+
+    website = WebsiteDetail.objects.get(linked_by="Website Details")
+
+    return render(request, 'index.html', {'website':website})
