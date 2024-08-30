59746cd51722024e7d6d22024a8995d570c88b1c
Sreedev Nair <sreedevnair02@gmail.com>
2024-08-30 23:49:45 +0530

    Jet for Django 3 Added

--- SreedevNair/urls.py
+++ SreedevNair/urls.py
@@ -20,5 +20,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('Main.urls')),
+    path('jet/', include('jet.urls', 'jet')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
